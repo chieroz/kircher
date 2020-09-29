ad4a5a6a13b5d996842d461534d1793816a0beaa
Carlo Mario Chierotti <carlomario@chierotti.it>
2020-09-29 16:20:15 +0200

    Update footnotes.js

--- static/js/footnotes.js
+++ static/js/footnotes.js
@@ -5,10 +5,8 @@
 
 $(document).ready(function () {
   $(".footnote-link").on("click", function () {
-    let fnNumber = $(this).data("footnote");
-    let fnText = $("#fn" + fnNumber).html();
-
-    console.log(fnText);
+    var fnNumber = $(this).data("footnote");
+    var fnText = $("#fn" + fnNumber).html();
 
     $("#myFootnoteNumber").html(fnNumber);
     $("#myFootnoteText").html(fnText);
